4c19c89e73b51627f2aec084d2665b3880393fdf
haithem satoutah <satoutahhaithem@gmail.com>
2023-03-24 22:55:27 +0100

    adding the raspberry code

---
+++ thisDirectoryOnRaspberry/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+def gen_frames():
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+    camera.release()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
